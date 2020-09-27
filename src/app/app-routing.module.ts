d1085259929730db0d5037d67fd15d8f41e5d3bd
Admin <raxo00012345@gmail.com>
2020-09-27 11:32:04 +0600

    Adding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BrowseComponent} from './components/browse/browse.component';
+import {AboutComponent} from './components/about/about.component';
+import {OrderComponent} from './components/order/order.component';
+import {AccountComponent} from './components/account/account.component';
+import {DiscountComponent} from './components/discount/discount.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: BrowseComponent},
+  {path: 'home' , component: BrowseComponent},
+  {path: 'about' , component: AboutComponent},
+  {path: 'order' , component: OrderComponent},
+  {path: 'account' , component: AccountComponent},
+  {path: 'service', component: DiscountComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DiscountComponent } from './components/discount/discount.component';
+import { BrowseComponent } from './components/browse/browse.component';
+import { OrderComponent } from './components/order/order.component';
+import { AccountComponent } from './components/account/account.component';
+import { AboutComponent } from './components/about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DiscountComponent,
+    BrowseComponent,
+    OrderComponent,
+    AccountComponent,
+    AboutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
